869fb3ac8a4e22fbd8e85357ce6b971d8d520305
paolaelenabernardelli <32967788+paolaelenabernardelli@users.noreply.github.com>
2018-08-09 12:31:56 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-
+<?php echo "It’s all right!";
+// recupero il contenuto inviato da Telegram
+$content=file_get_contents("php://input");
+// converto il contenuto da JSON ad array PHP
+$update=json_decode($content, true); 
+if(!$update) { 
+  exit; 
+} 
+$website="https://api.telegram.org/";
+$text=$update['message']['text'];
+$chatid=$update['message']['from']['id'];
+$agg=json_encode($update, JSON_PRETTY_PRINT);
+function sendMessage($chatId, $text) {
+  $api='bot542609682:AAEefoEGUUriQtztOG-Yf5LSetKv1EmpxyQ';
+  $url=$GLOBALS[$website].$api."/sendMessage?chatid=$chatid&$text=".urlencode($text); 
+  $get = file_get_contents($url);
+  }
+sendMessage($chatid,"Ciao");
+?>
